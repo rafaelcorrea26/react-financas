6d3324df0adfc463b422d5ff076aa8150474f1d6
rafaelcorrea26 <rafael.fs.camaqua@gmail.com>
2023-09-25 23:21:49 -0300

    main

---
+++ src/components/Form/InputRadioGroup/index.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import './styles.css';
+
+interface InputRadioGroupProps {
+    onChange: (valor: string) => void;
+    checked: boolean;
+
+}
+
+const InputRadioGroup = (props:InputRadioGroupProps) => {    
+            const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+                props.onChange( e.target.value);
+              }
+        
+
+    return (
+        <div onChange={onChange} className='radiog'>
+            <input type="radio" value="Entrada" name="tipo" checked={props.checked}/> Entrada
+            <input type="radio" value="Saida" name="tipo" /> Saida
+        </div>
+    )}
+
+export default InputRadioGroup;
